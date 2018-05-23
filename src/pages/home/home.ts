1bb61be901d367bcff3e1b13d2d0ae818a1c1ce3
Leonardo Pricevicius <leonardo@okn.com.br>
2018-05-23 08:19:49 -0300

    usando plugin foto

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,42 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[
+    Camera
+  ]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  img = "";
+
+  constructor(
+    public navCtrl: NavController,
+    private camera: Camera) {
 
   }
 
+  tirarFoto(){
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+    
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64:
+     this.img = 'data:image/jpeg;base64,' + imageData;
+    }, (err) => {
+     // Handle error
+    });
+  }
+
+  ionViewDidEnter() {
+   
+  }
 }
